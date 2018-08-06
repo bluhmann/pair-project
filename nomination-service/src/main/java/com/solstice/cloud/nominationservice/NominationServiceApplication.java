6a2859ddbcd316f1082120c944b4b13a9153d448
Bill Luhmann <bluhmann@solstice-consulting.com>
2018-08-06 17:27:01 -0500

    First initial commit -- setup services and discovery base projects

---
+++ nomination-service/src/main/java/com/solstice/cloud/nominationservice/NominationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.cloud.nominationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominationServiceApplication.class, args);
+	}
+}
