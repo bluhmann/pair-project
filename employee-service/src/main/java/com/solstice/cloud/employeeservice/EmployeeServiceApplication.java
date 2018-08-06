9054e764d2e6a8238ba7fb1765383aa275313e95
Bill Luhmann <bluhmann@solstice-consulting.com>
2018-08-06 17:46:18 -0500

    add config stuff

--- employee-service/src/main/java/com/solstice/cloud/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/solstice/cloud/employeeservice/EmployeeServiceApplication.java
@@ -2,8 +2,10 @@ package com.solstice.cloud.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
